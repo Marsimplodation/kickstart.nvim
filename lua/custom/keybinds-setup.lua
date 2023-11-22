3b08b56e1449303eca04d376ceb4205cbc3dac00
marsimplodation <naroxlp@gmail.com>
2023-11-22 21:56:02 +0100

    nvim config

---
+++ lua/custom/keybinds-setup.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<leader>e", ':Neotree<CR>')
+vim.keymap.set("n", "<leader>w", ':w<CR>')
+vim.keymap.set("n", "<leader>q", ':q<CR>')
+vim.keymap.set("n", "<F5>", ":!(make run)<CR>")
